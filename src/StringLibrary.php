e3ef896a0a5e4958b351ce819e89204c4bf8df60
Pendant <861618191@qq.com>
2019-05-29 22:33:42 +0800

    新增身份证合法性校验

--- src/StringLibrary.php
+++ src/StringLibrary.php
@@ -8,6 +8,45 @@ namespace functionsLibrary;
  */
 class StringLibrary
 {
+     /**
+     * 校验身份证合法性
+     * @param string $string           18位身份证
+     * @return bool
+     */
+    public function checkIdCard(string $string) 
+    {
+        $string = strtoupper($string);
+        $regx = "/(^\d{17}([0-9]|X)$)/";
+        $arr_split = array();
+        if(!preg_match($regx, $string))
+        {
+            return false;
+        }
+
+        $regx = "/^(\d{6})+(\d{4})+(\d{2})+(\d{2})+(\d{3})([0-9]|X)$/";
+        preg_match($regx, $string, $arr_split);
+        $dtm_birth = $arr_split[2] . '/' . $arr_split[3]. '/' .$arr_split[4];
+        if(!strtotime($dtm_birth)) { //检查生日日期是否正确
+            return false;
+        }
+        //检验18位身份证的校验码是否正确。
+        //校验位按照ISO 7064:1983.MOD 11-2的规定生成，X可以认为是数字10。
+        $arr_int = array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2);
+        $arr_ch = array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2');
+        $sign = 0;
+        for ( $i = 0; $i < 17; $i++ ) {
+            $b = (int) $string{$i};
+            $w = $arr_int[$i];
+            $sign += $b * $w;
+        }
+        $n = $sign % 11;
+        $val_num = $arr_ch[$n];
+        if ($val_num != substr($string,-1)) {
+            return false;
+        }
+        return true;
+    }
+
     /**
      * 随机字符串
      * @param int $length           长度
